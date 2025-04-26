13233e96058cd41184e19833d58789edd936d105
Miles Bellaire <mile5b311@gmail.com>
2025-04-25 21:24:04 -0500

    Saving before migration

--- src/Components/WorkExperience.js
+++ src/Components/WorkExperience.js
@@ -5,13 +5,25 @@ import { useEffect, useState } from 'react';
 
 export default function WorkExperience({Ref, opacity=1}) {
    const [workExperience, setWorkExperience] = useState([]);
+   const defaultExperiences = [
+      {
+         name: 'Not Available',
+         description: 'Not Available',
+         tags: 'Not Available',
+         link: 'Not Available',
+         image: 'Not Available',
+         currentPosition: 'Not Available',
+         startDate: 'Not Available',
+         endDate: 'Not Available',
+         position: 'Not Available'  
+      }
+   ]
 
    useEffect(() => {
       const getItems = async () => {
          const response = await ExperienceService.GetWorkExperience();
          if(response.error) return;
-
-         setWorkExperience(response);
+         setWorkExperience(defaultExperiences);
       }
 
       getItems();
--- todo.md
+++ todo.md
@@ -4,3 +4,4 @@
 - add show more button in experience component
 - setup all of backend
 - fix weird spacing on contact me when smaller size
+- Update npm
